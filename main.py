7783effc012c36c3ccfc25c5b8274f5b5c45b48c
Alex <46222337+Macrou@users.noreply.github.com>
2024-04-29 18:27:10 +0200

    modified the fakedditDataLoader so no more unarchive

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ transform_test = transforms.Compose([
     transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
 ])
 
-trainset = Fakeddit("multimodal_only_samples/multimodal_train.tsv","public_images.tar2.bz2 ") 
+trainset = Fakeddit("multimodal_only_samples/multimodal_train.tsv","public_images.tar.bz2 ") 
 trainloader = torch.utils.data.Dataloader(
     trainset,batch_size=128, shuffle=True, num_workers=2
 )

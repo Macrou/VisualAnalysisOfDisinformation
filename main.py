6792fb67701f917edeeb5e7265ae1b43d4b528d9
Alex <46222337+Macrou@users.noreply.github.com>
2024-05-14 20:53:47 +0200

    added more workers

--- main.py
+++ main.py
@@ -43,12 +43,12 @@ transform_test = transforms.Compose([
 
 trainset = datasets.ImageFolder(root='dataset/cifake/train',transform=transform_train)
 trainloader = torch.utils.data.DataLoader(
-    trainset,batch_size=128, shuffle=True, num_workers=2
+    trainset,batch_size=128, shuffle=True, num_workers=4
 )
 
 testset = datasets.ImageFolder(root='dataset/cifake/test',transform=transform_test)
 testloader = torch.utils.data.DataLoader(
-    testset,batch_size=100, shuffle=False, num_workers=2
+    testset,batch_size=100, shuffle=False, num_workers=4
 )
 
 classes = ('false','real')

900895000e557867296bf00d8ef4a96d4db5a637
U-CALIBARN\alexa <alexandru.m.chirita@gmail.com>
2024-05-14 18:38:59 +0200

    cifar test

--- main.py
+++ main.py
@@ -33,20 +33,20 @@ transform_train = transforms.Compose([
     transforms.RandomCrop(32, padding=4),
     transforms.RandomHorizontalFlip(),
     transforms.ToTensor(),
-    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    #transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
 ])
 
 transform_test = transforms.Compose([
     transforms.ToTensor(),
-    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    #transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
 ])
 
-trainset = datasets.ImageFolder(root='dataset/cifake/train')
+trainset = datasets.ImageFolder(root='dataset/cifake/train',transform=transform_train)
 trainloader = torch.utils.data.DataLoader(
     trainset,batch_size=128, shuffle=True, num_workers=2
 )
 
-testset = datasets.ImageFolder(root='dataset/cifake/test')
+testset = datasets.ImageFolder(root='dataset/cifake/test',transform=transform_test)
 testloader = torch.utils.data.DataLoader(
     testset,batch_size=100, shuffle=False, num_workers=2
 )

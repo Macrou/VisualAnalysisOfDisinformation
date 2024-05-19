d409a1de4204df26c41f179df3efd2e6f2defb47
Alex <46222337+Macrou@users.noreply.github.com>
2024-05-19 17:33:19 +0200

    modified dataloader

--- fakedditDataLoader.py
+++ fakedditDataLoader.py
@@ -9,27 +9,34 @@ import io
 class Fakeddit(Dataset):
 
     def __init__(self,annotations_file, img_dir="dataset/public_images.tar.bz2", transform=None, target_transform=None):
-        self.img_labels = pd.read_csv(annotations_file,sep='\t')
+        self.id = pd.read_csv(annotations_file,sep='\t',index_col= 'id').index.values
         self.img_dir = img_dir
+        self.img_labels = pd.read_csv(annotations_file,sep='\t',index_col= '2_way_label').index.values
         self.transform = transform
         self.target_transform = target_transform
+        self.tf = tarfile.open(self.img_dir,'r:bz2')
+
+    def get_image_from_tar(self, name):
+        """
+        Gets a image by a name gathered from file list csv file
+
+        :param name: name of targeted image
+        :return: a PIL image
+        """
+        image = self.tf.extractfile(name)
+        image = image.read()
+        image = Image.open(io.BytesIO(image))
+        return image
 
     def __len__(self):
         return len(self.img_labels)
 
     def __getitem__(self, idx):
-        img_dir_without_tar = self.img_dir[:-8]
-        img_path = os.path.join(img_dir_without_tar, self.img_labels.iloc[idx, 5])
-        with tarfile.open(self.img_dir, 'r:bz2') as tar:
-            try:
-                img_file = tar.extractfile(img_path)
-                if img_file is None:
-                    raise FileNotFoundError(f"{img_path} not found in the tar archive.")
-                img_data = img_file.read()
-                image = read_image(io.BytesIO(img_data))
-            except KeyError:
-                raise FileNotFoundError(f"{img_path} not found in the tar archive.")
-        label = self.img_labels.iloc[idx, 13]
+        print(self.img_labels[0])
+        if idx == (self.__len__() - 1) :  # close tarfile opened in __init__
+            self.tf.close()
+        image = self.get_image_from_tar(self.id[idx])
+        label = self.img_labels[idx]
         if self.transform:
             image = self.transform(image)
         if self.target_transform:

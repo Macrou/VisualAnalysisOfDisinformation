0d76e00306f3cf68d948bfeb236d7c77ba04a2af
Alex <46222337+Macrou@users.noreply.github.com>
2024-06-13 22:13:36 +0200

    Refactored fakeddit_data_analyser.py

--- fakeddit_data_analyser.py
+++ fakeddit_data_analyser.py
@@ -1,35 +1,137 @@
-import pandas as pd
-from PIL import Image
-import os
-import logging as log
-import matplotlib.pyplot as plt
+"""
+This file cleans the data sets of faulty images,
+and analyses the training set for corelation between title and label.
+"""
 
-def cleanData(annotations_file,new_annotations_file ,img_dir="dataset/public_image_set"):
+import os
+from collections import Counter
+
+import pandas as pd
+from PIL import Image,UnidentifiedImageError
+import matplotlib.pyplot as plt
+import seaborn as sns
+from wordcloud import WordCloud
+
+
+def clean_data(annotations_file,new_annotations_file ,img_dir="dataset/public_image_set"):
+    """Cleans the data set of faulty images.Checks if there is an exception and
+    if it exists it removes the image from the dataset.
+    At the end it creates a new annotation file containing only valid images.
+
+    Args:
+        annotations_file (string): the annotation file of the data set
+        new_annotations_file (string): the path for the new annotation file when created.
+        img_dir (str, optional): The path to the image directory of the dataset.
+        Defaults to "dataset/public_image_set".
+    """
     error_count = 0
     annotations = pd.read_csv(annotations_file,sep='\t',index_col='id')
-    ids = annotations['id'].values
-    for id in ids:
-        path = os.path.join(img_dir,id)
+    indices = annotations['id'].values
+    for index in indices:
+        path = os.path.join(img_dir,index)
         try:
-           img = Image.open(path)
-           img.verify()
-        except (IOError, SyntaxError) as e:
-            log.error(f"Error loading image {path}: {e}")
-            annotations.drop(index=id)
+            img = Image.open(path)
+            img.verify()
+        except (UnidentifiedImageError, OSError, IOError) as e:
+            print(f"Error loading image {path}: {e}")
+            annotations.drop(index=index,inplace=True)
             error_count += 1
-    log.info("Saving data into new CSV")
-    log.info(f"Removed {error_count} images")
+    print("Saving data into new CSV")
+    print(f"Removed {error_count} images")
     annotations.to_csv(new_annotations_file,sep='\t')
 
-def EDA(annotations_file,img_dir="dataset/public_image_set"):
+def label_distribution(df):
+    """Plots the distribution of the 2_way_label
+
+    Args:
+        df (pd.DataFrame): the Data Frame used for this plot
+    """
+    df['2_way_label'].value_counts().plot(kind='bar')
+    plt.title('Distribution of 2_way_label')
+    plt.xlabel('Label')
+    plt.ylabel('Frequency')
+    plt.savefig(fname='results/plots/labelFrequency.png',format='png')
+
+def title_analyzer(df):
+    """Plots the word cloud with the most common words in the clean titles of the dataset,
+    and prints 10 of the most common words in the title
+
+    Args:
+        df (_type_): _description_
+    """
+    wordcloud = WordCloud(width=800, height=400,
+                          background_color='white').generate(' '.join(df['clean_title']))
+    plt.imshow(wordcloud, interpolation='bilinear')
+    plt.axis('off')
+    plt.savefig(fname='results/plots/wordCloud.png',format='png')
+    df['title_length'] = df['clean_title'].apply(len)
+    word_counts = Counter(" ".join(df['clean_title']).split())
+    print(word_counts.most_common(10))
+
+def word_cloud_for_positive_and_negative_labels(df):
+    """Generates the word cloud for true images and fake images.
+
+    Args:
+        df (_type_): _description_
+    """
+    positive_titles = df[df['2_way_label'] == 1]['clean_title']
+    negative_titles = df[df['2_way_label'] == 0]['clean_title']
+
+    positive_wordcloud = WordCloud(width=800, height=400,
+                                   background_color='white').generate(' '.join(positive_titles))
+    negative_wordcloud = WordCloud(width=800, height=400,
+                                   background_color='white').generate(' '.join(negative_titles))
+
+    plt.figure(figsize=(15, 7))
+
+    plt.subplot(1, 2, 1)
+    plt.imshow(positive_wordcloud, interpolation='bilinear')
+    plt.axis('off')
+    plt.title('Positive Class Word Cloud')
+
+    plt.subplot(1, 2, 2)
+    plt.imshow(negative_wordcloud, interpolation='bilinear')
+    plt.axis('off')
+    plt.title('Negative Class Word Cloud')
+
+    plt.savefig(fname='results/plots/wordCloudPositiveAndNegative.png',format='png')
+
+
+def box_plot_corelation_between_label_and_title_length(df):
+    """Box plot title length and label.
+
+    Args:
+        df (data_frame): _description_
+    """
+    sns.boxplot(x='2_way_label', y='title_length', data=df)
+    plt.title('Title Length by 2_way_label')
+    plt.xlabel('2_way_label')
+    plt.ylabel('Title Length')
+    plt.savefig(fname='results/plots/boxPlot.png',format='png')
+
+
+def eda(annotations_file):
+    """performs exploratory data analysis on the dataset
+
+    Args:
+        annotations_file (_type_): the path to the annotation file
+    """
     annotations = pd.read_csv(annotations_file,sep='\t',index_col='id')
-    label_count = annotations['2_way_label'].value_counts()
-    plt.figure(figsize=(8, 6))
-    plt.bar(label_count.index,quality_counts)
-    plt.title('Count of 2 way label')
-    plt.xlabel('2 ') 
-    plt.ylabel('Count')
+    print(annotations.shape)
+    print(annotations.info())
+
+    label_distribution(annotations)
+    title_analyzer(annotations)
+    box_plot_corelation_between_label_and_title_length(annotations)
+    word_cloud_for_positive_and_negative_labels(annotations)
+
 
 
 if __name__ == "__main__":
-    cleanData()
+    clean_data(annotations_file="./dataset/multimodal_only_samples/multimodal_train.tsv",
+               new_annotations_file="./results/multimodal_only_samples/multimodal_train.tsv")
+    clean_data(annotations_file="./dataset/multimodal_only_samples/multimodal_test.tsv",
+               new_annotations_file="./results/multimodal_only_samples/multimodal_test.tsv")
+    clean_data(annotations_file="./dataset/multimodal_only_samples/multimodal_train.tsv",
+               new_annotations_file="./results/multimodal_only_samples/multimodal_validate.tsv")
+    eda(annotations_file="./results/multimodal_only_samples/multimodal_train.tsv")

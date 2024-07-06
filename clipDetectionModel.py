86d99ef2ccfac9b90cb66f91dd89539c11ded9a8
Alex <46222337+Macrou@users.noreply.github.com>
2024-07-06 23:21:49 +0200

    Added XGBoost implementation of random forest

--- clipDetectionModel.py
+++ clipDetectionModel.py
@@ -1,33 +1,23 @@
 import torch
-import torch.nn as nn
-import torch.optim as optim
-import torch.nn.functional as F
-import torch.backends.cudnn as cudnn
-
 import torch.utils
 from torch.utils.data import DataLoader
 from torchvision import datasets 
-import torchvision.transforms as transforms
 
 import clip
 from dataloaders.fakeddit_data_loader import Fakeddit
 
-import numpy as np
-import pandas as pd
+
 from tqdm import tqdm
-import matplotlib.pyplot as plt
-from sklearn.linear_model import LogisticRegression
-from sklearn.ensemble import RandomForestClassifier
 import os
 import argparse
 
-from algorithms.model_handler import ModelHandler
 from algorithms.model_factoy import ModelFactory
 
 from dataloaders.fakeddit_data_loader import *
 parser = argparse.ArgumentParser(description='PyTorch Disinformation Training')
 parser.add_argument('--data','-d', choices=['Fakeddit', 'CIFAKE'],default='Fakeddit', type= str, help='data set')
-parser.add_argument('--classifier','-c',choices=['Logistic', 'Random Forest','KNN'],default='Logistic',type= str, help='classifier used')
+parser.add_argument('--classifier','-c',choices=['Logistic', 'Random Forest','KNN','XGBoost'],default='Logistic',type= str, help='classifier used')
+#parser.add_argument('--mode','-m',choices=['Logistic', 'Random Forest','KNN',],default='Logistic',type= str, help='mode used')
 args = parser.parse_args()
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
@@ -63,5 +53,5 @@ if __name__ == "__main__":
     print('==> Getting features..')
     train_features, train_labels = get_features(train)
     test_features, test_labels = get_features(test)
-    model_handler = ModelFactory(train_features,train_labels,test_features,test_labels).create()
+    model_handler = ModelFactory(train_features,train_labels,test_features,test_labels,device).create()
     model_handler.train_model(args.classifier)
